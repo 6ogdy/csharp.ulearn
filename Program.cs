3b7140987babd58b32b79d9e6068231464b7f850
6ogdy <changingmylife.6ogdy@gmail.com>
2022-09-07 14:14:34 +0300

    Delete 2 lines

--- Program.cs
+++ Program.cs
@@ -26,10 +26,6 @@ namespace ulearn
             Console.WriteLine(GetMinX(5, 2, 1));
             Console.WriteLine(GetMinX(4, 3, 2));
             Console.WriteLine(GetMinX(0, 4, 5));
-
-            // А в этих случаях решение существует:
-            Console.WriteLine(GetMinX(0, 0, 2) != "Impossible");
-            Console.WriteLine(GetMinX(0, 0, 0) != "Impossible");
         }
     }
 }
