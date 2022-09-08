0cb2224afa5804fda9f29e96225fbd8c83c141d1
6ogdy <changingmylife.6ogdy@gmail.com>
2022-09-08 17:04:33 +0300

    Semaphore

---
+++ semaphore/Semaphore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace ulearn 
+{
+    internal class Semaphore
+    {
+        private static string GetMinX(int a, int b, int c)
+        {
+            if (a < 0)
+            {
+                return "Impossible";
+            }
+            else
+            {
+                double x = -(b / (2.0 * a));
+                return(x).ToString();
+            }
+            
+        }
+        //alio
+        static void Main()
+        {
+            Console.WriteLine(GetMinX(1, 2, 3));
+            Console.WriteLine(GetMinX(0, 3, 2));
+            Console.WriteLine(GetMinX(1, -2, -3));
+            Console.WriteLine(GetMinX(5, 2, 1));
+            Console.WriteLine(GetMinX(4, 3, 2));
+            Console.WriteLine(GetMinX(0, 4, 5));
+        }
+    }
+}
